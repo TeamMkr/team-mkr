273b88d84622c16ed2ba56b05480748006e45927
Renee <rchar20@gmail.com>
2017-01-06 23:36:09 -1000

    adds router and header
    - creates community, dashboard, and header routes
    - sets dashboard as default router outlet
    - creates basic header with nav bar
    
    references #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CommunityComponent } from './community/community.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'community', component: CommunityComponent }
+];
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		HeaderComponent,
+		ProfileComponent,
+		CommunityComponent,
+		DashboardComponent
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
-		HttpModule
+		HttpModule,
+    RouterModule.forRoot(appRoutes)
 	],
 	providers: [],
 	bootstrap: [AppComponent]
