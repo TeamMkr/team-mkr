68df522bfe4c7b652e7655f0f4b1458e1ceffb9c
FrozenPandaz <jasonjean1993@gmail.com>
2017-01-13 20:45:38 -0500

    Add OnPush to components (#19)

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
 	selector: 'community-page',
 	templateUrl: './community.component.html',
-	styleUrls: ['./community.component.scss']
+	styleUrls: ['./community.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CommunityComponent implements OnInit {
 
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
 	selector: 'profile-page',
 	templateUrl: './profile.component.html',
-	styleUrls: ['./profile.component.scss']
+	styleUrls: ['./profile.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileComponent implements OnInit {
 
