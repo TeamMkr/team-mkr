7a7840704107817c70898aeea7429a7f55d9fc26
Renee <rchar20@gmail.com>
2017-01-13 17:12:03 -1000

    changed spaces to tab indentation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'login-page',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'login-page',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
